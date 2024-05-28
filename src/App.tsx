733f6a34eec2fd8502760dd70d7a93fb891eff72
Antoine Wurtz <antoine.wurtz@crowdaa.com>
2024-05-28 20:47:26 +0200

    fix tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import NoteForm from './components/NoteForm';
 const App: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold m-4 text-center">Notes App</h1>
+      <h1 className="text-3xl font-bold mb-4">Notes App</h1>
       <NoteForm />
       <NoteList />
     </div>
